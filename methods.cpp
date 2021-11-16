434ebcc13fd36ee43604bd1e276e8fa5efab1bac
caiofov <cfoviana@gmail.com>
2021-11-16 00:31:25 -0300

    comenting and translating

--- methods.cpp
+++ methods.cpp
@@ -3,21 +3,21 @@
 using namespace std;
 #include "methods.h"
 
-float falsePosition(float a, float b, float precisao, float(*function)(float), int count){
+float falsePosition(float a, float b, float precision, float(*function)(float), int count){
   count++;
   float fa = function(a); //f(a)
   float fb = function(b); //f(b)
   
-  if( (abs(b-a) > precisao) && (fa*fb < 0)){
-    float pm = ((a*fb)-(b*fa))/(fb - fa); //ponto medio
-    float fp = function(pm); //f(ponto medio)
+  if( (abs(b-a) > precision) && (fa*fb < 0)){
+    float pm = ((a*fb)-(b*fa))/(fb - fa); //interception point
+    float fp = function(pm); //f(interception point)
         
-    if((fp*fa < 0) && (abs(pm - a) > precisao) && (abs(fp) > precisao)){
-      return falsePosition(a, pm, precisao, function, count);
+    if((fp*fa < 0) && (abs(pm - a) > precision) && (abs(fp) > precision)){
+      return falsePosition(a, pm, precision, function, count);
     }
     
-    else if((fp * fb < 0) && (abs(b-pm) > precisao) && (abs(fp) > precisao)){
-      return falsePosition(pm, b, precisao, function, count);
+    else if((fp * fb < 0) && (abs(b-pm) > precision) && (abs(fp) > precision)){
+      return falsePosition(pm, b, precision, function, count);
     }
     
     else{
@@ -30,7 +30,7 @@ float falsePosition(float a, float b, float precisao, float(*function)(float), i
   }
 }
 
-float bissection(float a, float b, float precisao, float(*function)(float), int count){
+float bissection(float a, float b, float precision, float(*function)(float), int count){
   count++;
   float fa = function(a); //f(a)
   float fb = function(b); //f(b)
@@ -42,20 +42,19 @@ float bissection(float a, float b, float precisao, float(*function)(float), int 
     return b;
   }
   
-  if( (abs(b-a) > precisao) && (fa*fb < 0)){
-    float pm = (a+b)/2; //ponto medio
-    float fp = function(pm); //f(ponto medio)
-
+  if( (abs(b-a) > precision) && (fa*fb < 0)){
+    float pm = (a+b)/2; //middle of the interval
+    float fp = function(pm); //f(middle)
     if((fp*fa < 0) 
-    && (abs(pm - a) > precisao) 
-    && (abs(fp) > precisao)){
-      return bissection(a, pm, precisao, function, count);
+    && (abs(pm - a) > precision) 
+    && (abs(fp) > precision)){
+      return bissection(a, pm, precision, function, count);
     }
     
     else if((fp * fb < 0) 
-    && (abs(b-pm) > precisao) 
-    && (abs(fp) > precisao)){
-      return bissection(pm, b, precisao, function, count);
+    && (abs(b-pm) > precision) 
+    && (abs(fp) > precision)){
+      return bissection(pm, b, precision, function, count);
     }
     
     else{
@@ -71,17 +70,16 @@ float bissection(float a, float b, float precisao, float(*function)(float), int 
   }
 }
 
-float newtonRaphson(float x0, float precisao, float (*function)(float), float (*functionderivative)(float), int count){
+float newtonRaphson(float x0, float precision, float (*function)(float), float (*functionDerivative)(float), int count){
   count++;
-  printf("\n%dª Iteração \n", count);
   float fx0 = function(x0);
   float phiX = x0 - (fx0/ functionDerivative(x0));
   
   if(fx0 == 0){ //checks if it's an exact root
     return x0;}; 
   
-  if(abs(fx0) > precisao){
-    return newtonRaphson(phiX, precisao, function, functionDerivative, count);
+  if(abs(fx0) > precision){
+    return newtonRaphson(phiX, precision, function, functionDerivative, count);
   }
   else{
     return x0;
@@ -89,7 +87,7 @@ float newtonRaphson(float x0, float precisao, float (*function)(float), float (*
 
 }
 
-float secant(float x0, float x1, float precisao, float (*function)(float), float (*functionderivative)(float), int count){
+float secant(float x0, float x1, float precision, float (*function)(float), float (*functionDerivative)(float), int count){
   count++;
   
   float fx0 = function(x0);
@@ -97,15 +95,15 @@ float secant(float x0, float x1, float precisao, float (*function)(float), float
   
   float phiX2 = ((x0*fx1) - x1*fx0)/(fx1 - fx0);
 
-  if(fx0 == 0){ //checa se é raiz
+  if(fx0 == 0){ //checks if it's an exact root
     return x0;
     }; 
-  if(fx1 == 0){ //checa se é raiz
+  if(fx1 == 0){ //checks if it's an exact root
     return x1;
   } 
 
-  if(abs(fx0) > precisao){
-    return secant(x1,phiX2,precisao, function, functionDerivative, count);
+  if(abs(fx0) > precision){
+    return secant(x1,phiX2,precision, function, functionDerivative, count);
   }
 
   else{
@@ -115,52 +113,51 @@ float secant(float x0, float x1, float precisao, float (*function)(float), float
 
 }
 
-float pontoFixo(float x0, float (*function)(float), float (*functionIteration)(float), float precisao, int count){
+float pontoFixo(float x0, float (*function)(float), float (*functionIteration)(float), float precision, int count){
   count++;
 
   float phix0 = functionIteration(x0);
   float fx0 =  function(x0);
 
-  if(fx0 == 0){ //checa se x0 é raiz
-    cout << "x0 é raiz" << endl;
+  if(fx0 == 0){ //checks if it's an exact root
     return x0;
   }
 
-  if( abs(fx0) > precisao){
+  if( abs(fx0) > precision){
     
-    return pontoFixo(phix0, function, functionIteration, precisao, count);
+    return pontoFixo(phix0, function, functionIteration, precision, count);
   }
 
   if(isinf(x0) || isnan(x0)){
-    cout << "Tip: try another iteration function" << endl;
+    printf("Tip: try another iteration function");
   }
   
   return x0;
   
 }
 
-float calculoPolinomio(int numCoef, float coeficientes[numCoef], float x){
-  int grau = numCoef - 1;
+float calculatePolynomial(int coefNum, float coeficients[], float x){
+  int degree = coefNum - 1;
   float z = x;
 
-  float b = coeficientes[grau];
+  float b = coeficients[degree];
   
-  for(int i = grau-1; i > -1; i--){
-    b = coeficientes[i] + b*z;
+  for(int i = degree-1; i > -1; i--){
+    b = coeficients[i] + b*z;
   }
 
   return b;
 }
 
-float calculateDerivative(int numCoef, float coeficientes[numCoef], float x){
+float calculateDerivative(int coefNum, float coeficientes[], float x){
   
-  int grau = numCoef - 1;
+  int degree = coefNum - 1;
   float z = x;
 
-  float b = coeficientes[grau];
+  float b = coeficientes[degree];
   float c = b;
   
-  for(int i = grau-1; i > 0; i--){
+  for(int i = degree-1; i > 0; i--){
     b = coeficientes[i] + b*z;
     c = b + c*z;
   }
@@ -169,35 +166,33 @@ float calculateDerivative(int numCoef, float coeficientes[numCoef], float x){
   
 }
 
-float polinomioNewton(int numCoef, float coeficientes[numCoef], float x, float precisao, int maxIter){
-  int grau = numCoef - 1;
+float polynomialNewton(int coefNum, float coeficients[], float x, float precision, int maxIter){
+  int degree = coefNum - 1;
   float b,c;
 
   for(int k = 0; k < maxIter + 1; k++){
     
-    //inicializa as variaveis B e C
-    b = coeficientes[grau];
+    //initializing B and C variables
+    b = coeficients[degree];
     c = b;
     
-    for(int i = grau-1; i > 0; i--){
-      b = coeficientes[i] + b*x;
+    for(int i = degree-1; i > 0; i--){
+      b = coeficients[i] + b*x;
       c = b + c*x;
     }
-    b = coeficientes[0] + b*x;
+    b = coeficients[0] + b*x;
 
-    //mostrando os valores
-    printf("\n%dª Iteração: \nb: %f | c: %f \nx: %f\n", k+1,b,c, x);
 
-    //b = f(x) -> vendo se x já é um bom valor
-    if(abs(b) <= precisao){
+    //b = f(x) -> checks if x is already a good value for a root
+    if(abs(b) <= precision){
       return x;
     }
 
-    //atualizando o valor de X pela função de iteração do método de newton
+    //updating X by the iteration function of the original newton method
     x = x - b/c;
   }
 
-  cout << "Número máximo de iterações" << endl;
+  cout << "Max number of iterations reached" << endl;
   return -99999;
 
 
--- methods.h
+++ methods.h
@@ -17,16 +17,28 @@ float bissection(float a, float b, float precision, float (*function)(float), in
 - __x0__ - the first guess for the root
 - __precision__ - precision for the method. It's used to stop the iterations 
 - __function__ - the function which we want to find the roots
-- __functionDerivative__ - the derivative for the function used count - counter for iterations (default 0)
+- __functionDerivative__ - the derivative for the function used
 - __count__ - counter for iterations (default 0) */
 float newtonRaphson(float x0, float precision, float(*function)(float), float (*functionDerivative)(float), int count = 0);
 
+/* ###Secant method
+- __x0__ and __x1__ - Initial guesses for the root
+- __precision__ - precision for the method. It's used to stop the iterations 
+- __function__ - the function which we want to find the roots
+- __functionDerivative__ - the derivative for the function used
+- __count__ - counter for iterations (default 0) */
 float secant(float x0, float x1, float precision, float (*function)(float), float (*functionDerivative)(float), int count = 0);
 
 //questao 3
 float pontoFixo(float, float (*function)(float), float (*functionIteration)(float), float, int count = 0);
 
-float calculoPolinomio (int, float[], float);
+
+/*Returns the value of a function for a specific value.
+- __coefNum__ - Number of coeficients of the given function
+- __coeficients__ - An array with all coeficients (including 0's)
+- __x__ - the specific value for calculating the function
+*/
+float calculatePolynomial(int coefNum, float coeficients[], float x);
 
 /*Returns the derivative of a function for a specific value.
 - __coefNum__ - Number of coeficients of the given function
@@ -35,4 +47,11 @@ float calculoPolinomio (int, float[], float);
 */
 float calculateDerivative (int coefNum, float coeficients[], float x);
 
-float polinomioNewton(int, float coeficientes[], float x, float, int maxIter = 50);
+/*Returns a root of a function
+- __coefNum__ - Number of coeficients of the given function
+- __coeficients__ - An array with all coeficients (including 0's)
+- __x__ - the initial guess for the root
+- __precision__ - precision for the method. It's used to stop the iterations 
+- __maxIter__ - maximum number of iterations (defaul 50)
+*/
+float polynomialNewton(int coefNum, float coeficients[], float x, float precison, int maxIter = 50);
